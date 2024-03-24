59b1c44e1d4be2455416300d85e8810bf170d044
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-03-24 00:05:11 -0300

    Resolviendo las relaciones uno a uno

--- db/models/index.js
+++ db/models/index.js
@@ -9,6 +9,7 @@ function setupModels(sequelize) {
   Category.init(CategorySchema, Category.config(sequelize));
   Customer.init(CustomerSchema, Customer.config(sequelize));
 
+  User.associate(sequelize.models);
   Customer.associate(sequelize.models);
 }
 
