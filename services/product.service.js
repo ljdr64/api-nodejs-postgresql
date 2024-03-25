e8e1d6b7fb5c545f2ccce1d1bdb87dfe919071a9
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-03-25 05:03:11 -0300

    Filtrando precios con operadores

--- services/product.service.js
+++ services/product.service.js
@@ -1,8 +1,8 @@
 const { faker } = require('@faker-js/faker');
+const { Op } = require('sequelize');
 const boom = require('@hapi/boom');
 
 const { models } = require('../libs/sequelize');
-const { query } = require('express');
 
 class ProductsService {
   constructor() {
@@ -31,12 +31,27 @@ class ProductsService {
   async find(query) {
     const options = {
       include: ['category'],
+      where: {},
     };
     const { limit, offset } = query;
     if (limit && offset) {
       options.limit = limit;
       options.offset = offset;
     }
+
+    const { price } = query;
+    if (price) {
+      options.where.price = price;
+    }
+
+    const { price_min, price_max } = query;
+    if (price_min && price_max) {
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max,
+      };
+    }
+
     const products = await models.Product.findAll(options);
     return products;
   }
